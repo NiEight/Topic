eb1d9b020efe2d05a804043c6f7704498b9e8afd
mojitoC <wjdgks8956@naver.com>
2022-06-17 18:15:57 +0900

    로그인 (구글, 카카오) 버튼 활성화, xml 수정 메인화면과 연결

--- app/src/main/java/com/example/topic/LoginActivity.java
+++ app/src/main/java/com/example/topic/LoginActivity.java
@@ -1,16 +1,199 @@
 package com.example.topic;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends Activity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.kakao.sdk.user.UserApiClient;
+import com.kakao.sdk.user.model.Account;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText idText, pwText;
+    Button loginKakao, loginButton;
+
+    GoogleSignInClient mGoogleSignInClient;
+    private final int RC_SIGN_IN = 123;
+    private static final String TAG = "LoginActivity";
+    private static final String TAG2 = "사용자";
+    SignInButton signBt;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+
+        idText = findViewById(R.id.idText);
+        pwText = findViewById(R.id.pwText);
+        loginKakao = findViewById(R.id.login_kakao);
+        loginButton = findViewById(R.id.loginBtn);
+
+        signBt = findViewById(R.id.login_google);
+        signBt.setOnClickListener(this::onClick);
+
+        // 카카오 로그인
+        loginKakao.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                UserApiClient.getInstance().loginWithKakaoTalk(LoginActivity.this,(oAuthToken, error) -> {
+                    if (error != null) {
+                        Log.e(TAG2, "로그인 실패", error);
+                    } else if (oAuthToken != null) {
+                        Log.i(TAG2, "로그인 성공(토큰) : " + oAuthToken.getAccessToken());
+
+                        UserApiClient.getInstance().me((user, meError) -> {
+                            if (meError != null) {
+                                Log.e(TAG2, "사용자 정보 요청 실패", meError);
+                            } else {
+                                System.out.println("로그인 완료");
+                                Log.i(TAG2, user.toString());
+                                {
+                                    Log.i(TAG2, "사용자 정보 요청 성공" +
+                                            "\n회원번호: "+user.getId() +
+                                            "\n이메일: "+user.getKakaoAccount().getEmail());
+                                }
+                                Account user1 = user.getKakaoAccount();
+                                System.out.println("사용자 계정" + user1);
+                            }
+                            return null;
+                        });
+                    }
+                    return null;
+                });
+
+            }
+        });
+        // 구글 로그인
+        // 앱에 필요한 사용자 데이터를 요청하도록 로그인 옵션을 설정한다.
+        // DEFAULT_SIGN_IN parameter는 유저의 ID와 기본적인 프로필 정보를 요청하는데 사용된다.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail() // email addresses도 요청함
+                .build();
+
+        // 위에서 만든 GoogleSignInOptions을 사용해 GoogleSignInClient 객체를 만듬
+        mGoogleSignInClient = GoogleSignIn.getClient(LoginActivity.this, gso);
+
+        // 기존에 로그인 했던 계정을 확인한다.
+        GoogleSignInAccount gsa = GoogleSignIn.getLastSignedInAccount(LoginActivity.this);
+
+        // 로그인 되있는 경우 (토큰으로 로그인 처리)
+        if (gsa != null && gsa.getId() != null) {
+
+        }
+        // 구글 로그인 끝
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount acct = completedTask.getResult(ApiException.class);
+
+            if (acct != null) {
+                String personName = acct.getDisplayName();
+                String personGivenName = acct.getGivenName();
+                String personFamilyName = acct.getFamilyName();
+                String personEmail = acct.getEmail();
+                String personId = acct.getId();
+                Uri personPhoto = acct.getPhotoUrl();
+
+                Log.d(TAG, "handleSignInResult:personName "+personName);
+                Log.d(TAG, "handleSignInResult:personGivenName "+personGivenName);
+                Log.d(TAG, "handleSignInResult:personEmail "+personEmail);
+                Log.d(TAG, "handleSignInResult:personId "+personId);
+                Log.d(TAG, "handleSignInResult:personFamilyName "+personFamilyName);
+                Log.d(TAG, "handleSignInResult:personPhoto "+personPhoto);
+            }
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.e(TAG, "signInResult:failed code=" + e.getStatusCode());
+
+        }
+
+
     }
 
+    public void onClick(View v) {
+        if (v.getId() == R.id.login_google) {
+            signIn();
+
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    public String getKeyHash(){
+        try{
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            if(packageInfo == null) return null;
+            for(Signature signature: packageInfo.signatures){
+                try{
+                    MessageDigest md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    return android.util.Base64.encodeToString(md.digest(), Base64.NO_WRAP);
+                }catch (NoSuchAlgorithmException e){
+                    Log.w("getKeyHash", "Unable to get MessageDigest. signature="+signature, e);
+                }
+            }
+        }catch(PackageManager.NameNotFoundException e){
+            Log.w("getPackageInfo", "Unable to getPackageInfo");
+        }
+        return null;
+    }
 
 }
+//google 로그인 관련 코드 참조
+// https://github.com/keepseung/Android-Blog-Source/blob/master/googlelogin/app/src/main/java/com/example/googlelogin/MainActivity.java
+//카카오 로그인 관련 코드
+//https://github.com/pu1et/kakaologin-android-sample/blob/master/app/src/main/java/com/kakaologin_sample/MainActivity.java
+
+
+
+
