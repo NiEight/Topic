9698c827e5b79b7d3bd001f4bb32b2aba12e76a5
mojitoC <wjdgks8956@naver.com>
2022-06-18 20:22:35 +0900

    리트

--- app/src/main/java/com/example/topic/CustomAdapter.java
+++ app/src/main/java/com/example/topic/CustomAdapter.java
@@ -139,10 +139,13 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
                     case 1001:  //콘텐츠 숨기기 클릭시
                     break;
                     case 1002:
+                        /*
                         int pos = getAdapterPosition();
-                        Intent intent = new Intent(context ,UtubePlay.class);
+                        Intent intent = new Intent(context ,Bookmark.class);
                         intent.putExtra("id", arrayList.get(pos).getVidold());
                         break;
+                        */
+
                 }
                 return false;
             }
